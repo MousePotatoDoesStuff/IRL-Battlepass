47290df14bd7381d0f17ed9dd653e790adad1057
Mouse Potato Does Stuff <mousepotato@tutanota.com>
2025-03-09 21:10:06 +0100

    wasd

--- Scenes/Notes/notes.gd
+++ Scenes/Notes/notes.gd
@@ -1,26 +1,70 @@
 extends MenuMode
 
-
-
+@export var nametext:RichTextLabel
+@export var notetext:TextEdit
+var all_notes:Array[String]=["Main\nType your notes here."]
+var current_note:int=0
 func on_open(in_data:Dictionary):
+	self.show()
 	self.data=in_data
-	$TextEdit.text=self.data['notes']
-	show()
-	return
+	var notes=self.data["notes"]
+	if typeof(notes)==typeof("string"):
+		self.all_notes=["Main"+"\n"+notes]
+	elif typeof(notes)==typeof([]):
+		self.all_notes=notes
+	else:
+		assert(false)
+	self.showNote(0)
 
+func showNote(current_note:int):
+	var size=len(self.all_notes)
+	if current_note<0:
+		current_note+=size
+	elif current_note>=size:
+		current_note-=size
+	current_note%=len(self.all_notes)
+	self.current_note=current_note
+	notetext.text=self.all_notes[self.current_note]
+	showName()
+
+func showName():
+	nametext.text="[center]"+notetext.text.split("\n")[0]
+	nametext.text+=" "+str(self.current_note+1)+"/"+str(len(self.all_notes))
+	
+	
 func on_close(_in_data:Dictionary):
 	on_edited()
 	hide()
 	return
 
 func on_edited():
-	if self.data['notes']==$TextEdit.text:
+	showName()
+	if self.all_notes[self.current_note]==notetext.text:
 		return
-	self.data['notes']=$TextEdit.text
+	self.all_notes[self.current_note]=notetext.text
 	quicksave.emit()
 
 func save_data(data_storage:Dictionary={})->Dictionary:
 	if data_storage.is_empty():
 		data_storage=self.data
-	data_storage['notes']=$TextEdit.text
+	self.all_notes[self.current_note]=notetext.text
 	return data_storage if data_storage!=null else {}
+
+func prev():
+	self.showNote(self.current_note-1)
+
+func next():
+	self.showNote(self.current_note+1)
+
+func new():
+	self.all_notes.append("New note")
+	self.showNote(self.current_note+1)
+
+func remove():
+	if len(self.all_notes)==1:
+		return
+	self.all_notes.remove_at(self.current_note)
+	if self.current_note==len(self.all_notes):
+		prev()
+		return
+	self.showNote(self.current_note)
